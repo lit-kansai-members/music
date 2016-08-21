daa8de21894c7ea9388c151097c2e0dd8058b245
Yugo Yagita <2000ygkn0713@gmail.com>
2016-08-21 19:02:01 +0900

    [add] Add task generate HTML.

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var gulp    = require("gulp");
+var $       = require("gulp-load-plugins")();
+
+gulp.task("generate", ["generate:html"]);
+
+gulp.task("generate:html", ["markdown"], function(){
+  gulp.src([
+    './src/html/header.html',
+    './tmp/*.html',
+    //'./tmp/index.html',
+    './src/html/footer.html'
+  ])
+    .pipe($.concat("index.html"))
+    .pipe(gulp.dest("./doc"));
+});
+
+gulp.task("markdown", function(){
+  gulp.src("./*.md")
+    .pipe($.emojify())
+    .pipe($.markdown())
+    .pipe(gulp.dest("./tmp"))
+});
