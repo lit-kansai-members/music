afcdd833be791f7dd46a0df1b6bf46f0a2bdea75
Yugo Yagita <2000ygkn0713@gmail.com>
2017-02-27 20:06:53 +0900

    Fix navigation

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,139 +1,64 @@
-let i, waitTransition;
-let $headings = {
-  year: $("header, #main h2, footer"),
-  camp: $("#main h3")
-};
-
-let $scroller = $("html, body");
-
-let $container = $("#navigations");
-
-let $window = $(window);
-
-let waitMouseMove = waitTransition = null;
-
-let headingTops = (() => {
-  let result = [];
-  for (i = 0; i < $headings.year.length; i++) {
-    let el = $headings.year[i];
-    result.push($(el).offset().top);
-  }
-  return result;
-})();
-
-headingTops.shift(0);
-
-
-let close = function(e){
-  $container
-  .find(".year.opened")
-  .removeClass("opened");
-};
-
-let open = function(index){
-  close();
-  $container
-  .find(`.year:eq(${index})`)
-  .addClass("opened")
-};
-
-
-$container
-.on("mouseenter", ".inneryear, .outerCamp", function() {
-  setTimeout(() => {
-  clearTimeout(waitMouseMove);
-  clearTimeout(waitTransition);
-  let $outer = $(this).closest(".year");
-  let $outerCamp = $outer.children(".outerCamp");
-  if (!$outerCamp.length) {
-    return;
-  }
-  let $lastCamp = $outerCamp.children(":last");
-  let height = $lastCamp.position().top + $lastCamp.innerHeight();
-
-  $outer.css({width: $outerCamp.innerWidth() + 2});
-  let t = $outer.css("transition-duration");
-  
-  return waitTransition = setTimeout(() =>
-    $outerCamp.css({
-      visibility: "visible",
-      height
-    })
-  
-  , parseFloat(t) * 1000);
-  },20)})
-.on("mouseleave", ".inneryear, .outerCamp", function() {
-  clearTimeout(waitTransition);
-  waitMouseMove = setTimeout(() => {
-    let $outer = $(this).closest(".year");
-    let $outerCamp = $outer.children(".outerCamp");
-    if (!$outerCamp.length) {
-      return;
-    }
-    
-    let t = $outerCamp
-    .css({height: 0})
-    .css({visibility: "hidden"})
-    .css("transition-duration");
-
-    waitTransition = setTimeout(() =>
-      $outer.css({width: ""})
-    , parseFloat(t) * 1000);
-  }, 15);
-})
-.on("click", "li", function() {
-  let target = this.classList.contains("year") ? "year" : "camp";
-  let index = $container.find(`li.${target}`)
-  .index(this);
-  let { top } = $headings[target]
-    .eq(index)
-    .offset();
-  $scroller.animate({scrollTop: top}, 200);
-  return false;}).children(".year")
-.each(function() {
-  let $this = $(this);
-  return $this.data("autoWidth",
-    $this
-    .children(".inneryear")
-    .innerWidth()
-  );
-});
-
-
-
-let opened = null;
-$window.on("scroll", function(e){
-  let scrollTop = $window.scrollTop();
-  let height = $window.height();
-  let seeing = headingTops.length;
-
-  for (i = 0; i < headingTops.length; i++) {
-    let top = headingTops[i];
-    if ((top - height) >= scrollTop) {
-      seeing = i;
-      break;
-    }
-  }
-
-  if (seeing !== opened) {
-    open(seeing);
-  }
-
-  return opened = seeing;
-});
-
-$window.trigger("scroll");
-
+/**
+ * .opened, .full-opened で最適な大きさになるようなCSSを作成して
+ * <head> の末尾に追記
+ */
+const style = document.createElement("style")
 let css = "";
 
-Array.from($container.children(".year")).forEach((el, i) =>{
-  const selector =`#navigations > .year:nth-child(${i + 1})`
+Array.from(document.querySelectorAll("#navigations .year"))
+.forEach((el, i) => {
+  const selector =`#navigations > .year:nth-child(${i + 1})`;
+  const children = Array.from(el.childNodes);
+  const inneryear = children.find(e => e.classList && e.classList.contains("inneryear"));
+  const outerCamp = children.find(e => e.classList && e.classList.contains("outerCamp"));
   css += `
 ${selector}.opened, ${selector}:hover {
-  width: ${$(el).children(".inneryear")[0].offsetWidth}px;
+  min-width: ${inneryear.offsetWidth + 2}px;
+}`;
+  if(outerCamp){
+    const camps = Array.from(outerCamp.childNodes).filter(e => e.classList && e.classList.contains("camp"));
+    const lastCamp = camps.pop();
+    css += `
+${selector}.full-opened {
+  width: ${outerCamp.offsetWidth + 2}px;
 }
-
+${selector}.full-opened .outerCamp {
+  height: ${lastCamp.offsetTop + lastCamp.offsetHeight}px;
+}
 `
+  }
+})
+
+style.innerHTML = css;
+document.head.appendChild(style);
+
+/**
+ * hover時に .full-opened をつける
+ */
+
+Array.from(document.querySelectorAll(".inneryear, .outerCamp"))
+.forEach( v => {
+  v.addEventListener("mouseenter", e =>
+    v.parentElement.classList.add("full-opened"));
+  v.addEventListener("mouseleave", e =>
+    v.parentElement.classList.remove("full-opened"));
 });
 
-$(document.head).append($("<style>" + css + "</style>"));
+/**
+ * マウスカーソルの .inneryaer → .outerCamp 移動時に
+ * .outerCampが閉じてしまうのを遅れることで回避
+ */
+
+Array.from(document.getElementsByClassName("outerCamp"))
+.forEach(outerCamp =>
+  outerCamp.addEventListener("transitionend", e=>{
+    const year = outerCamp.parentElement;
+    if(year.classList.contains("full-opened")) {
+      year.style.transitionDelay = "450ms";
+      outerCamp.style.transitionDelay = "50ms";
+    } else {
+      year.style.transitionDelay = "";
+      outerCamp.style.transitionDelay = "";
+    }
+  })
+);
