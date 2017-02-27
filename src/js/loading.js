dc9298e6c650b5d1c73e940eff38b226cd38eba0
Yugo Yagita <2000ygkn0713@gmail.com>
2017-02-28 00:00:49 +0900

    use svg animation close #40

--- src/js/loading.js
+++ src/js/loading.js
@@ -1,5 +1,21 @@
 $("#mark").css({
-  transform:`translateX(${ $("#loading img").offset().left - $("#mark").offset().left }px)`
+  transform:`translateX(${ $("#loading svg").offset().left - $("#mark").offset().left }px)`
 });
 
-$(window).on("load", () => $("body").addClass("loaded"));
+
+const paths = Array.from(document.querySelectorAll("path.first, path.second"));
+
+Promise.resolve()
+.then(new Promise(res => window.addEventListener("load", e => res())))
+.then(new Promise(res => 
+  paths[0].addEventListener("animationiteration", e =>{
+    if(!(e.elapsedTime % 4)) res()
+})))
+.then(new Promise(res =>{
+  paths.forEach(el => el.style.animationPlayState="paused")
+  const loading = document.getElementById("loading");
+  loading.style.opacity = "0";
+  loading.addEventListener("transitionend", e => res());
+}))
+.then(document.body.classList.add("loaded"))
+
