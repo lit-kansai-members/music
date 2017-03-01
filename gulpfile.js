a5010b1d3f3149be8366bf24e017974f85243ea6
Yugo Yagita <2000ygkn0713@gmail.com>
2017-03-01 23:30:22 +0900

    Fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,26 +5,27 @@ const $           = require("gulp-load-plugins")();
 const rimraf      =    require("rimraf");
 const browserSync =    require("browser-sync");
 const fs          =    require("fs");
+const YAML        = require("js-yaml");
 
-
-gulp.task("generate", ["clean","generate:html", "generate:js",
+gulp.task("generate", ["clean","generate:pug", "generate:js",
   "generate:scss", "images"], () =>
   gulp.src("src/gh-pages_README.md")
     .pipe($.rename("README.md"))
     .pipe(gulp.dest("build"))
 );
 
-gulp.task("generate:html", ["markdown", "chuiten"], () =>
-  gulp.src([
-    './src/html/header1.html',
-    './tmp/navigation.html',
-    './src/html/header2.html',
-    './tmp/index.html',
-    './src/html/footer1.html',
-    './tmp/chuiten.txt',
-    './src/html/footer2.html'
-  ])
-    .pipe($.concat("index.html"))
+gulp.task("generate:pug", () =>
+  gulp.src("./src/pug/index.pug")
+    .pipe($.data(f =>
+      ({data: {
+        marked: require("marked"),
+        years: YAML.safeLoad(fs.readFileSync("./index.yml")),
+        chuitens: fs.readdirSync("./注意点")
+          .filter(p => /.+\.md$/.test(p))
+          .map(p => ({content: fs.readFileSync("./注意点/"+p).toString(), name: p}))
+      }})
+    ))
+    .pipe($.pug({pretty: true}))
     .pipe(gulp.dest("./build"))
 );
 
@@ -41,108 +42,6 @@ gulp.task("generate:scss", () =>
     .pipe($.concat("style.css"))
     .pipe(gulp.dest("build/css/"))
 );
-
-gulp.task("markdown", ["youtube"], () =>
-  gulp.src("./tmp/index.md")
-    .pipe($.replace(/\[(.*)\]\(注意点\/(.+).md\)/g, '<a href="#$2" class="open-modal">$1</a>'))
-    .pipe($.markdown())
-    .pipe(gulp.dest("./tmp"))
-);
-
-gulp.task("youtube", function() {
-    const bgs = fs.readFileSync("./res/backgrounds.txt")
-      .toString()
-      .replace(/#.*$/gm, "")
-      .replace(/[ \t]/g, "")
-      .split("\n");
-    let bgcount = 0;
-    let nav = `<li class="year" data-year="↑">
-  <a class="inneryear smoothscroll" href="#">TOP</a>
-</li>`;
-    let yearTagOpened = false;
-
-    const res = fs.readFileSync("./index.md")
-      .toString()
-      .replace(/^##(#?)([^#\n]+)\n((?:(?:#{4,}[^\n]+|[^#\n]*)\n)*)/gm, function(match, isYear, name, body){
-        const id = name.toLowerCase().replace(/[!-/:-@¥[-`{-~\s]/g, "-");
-        isYear = isYear === "";
-        const str = `<h${isYear ? "2" : "3"} id=${id}>
-  <a href="#${id}">${name}</a>
-</h${isYear ? "2" : "3"}>`;
-        if (isYear) {
-          if (yearTagOpened) {
-            nav +="  </ul>\n</li>\n";
-          }
-          yearTagOpened = true;
-          const year = parseInt(name);
-          nav += `\
-<li class="year" data-year="${
-              !isNaN(year) ?
-                `${year.toString().slice(-2)}`
-              :
-                "★"
-              }">
-  <a class="inneryear smoothscroll" href="#${id}">${name}</a>
-  <ul class="outerCamp">\n\
-`;
-
-          return str + body;
-        } else {
-          nav += `    <li class='camp'><a href="#${id}" class="smoothscroll">${name}</a></li>\n`;
-          const bg = bgs[bgcount++];
-          return str +
-          `\
-<div class="parallax">
-  <img src="${bg || "./img/backgorunds/lifeistech-logo.png"}"
-    class="background${bg ? "" : " noimage"}">
-${
-          body.replace(/####([^#\n]+)\n(([^#][^#\n]+\n)*)/g, function(match, song, body) {
-            const result = /\[YouTube\]\((\/\/youtu\.be\/([\w-]+))\) {0,2}/.exec(body);
-            body = body.replace(/\[YouTube\]\((\/\/youtu\.be\/([\w-]+))\) {0,2}/, "");
-            const [author, title] = Array.from(song.split(" - "));
-
-            const youtube = result ?
-              fs.readFileSync("src/html/youtube.html")
-              .toString()
-              .replace(/{url}/g, result[1])
-              .replace(/{id}/g, result[2])
-            :
-              "";
-
-            return fs.readFileSync("src/html/song.html")
-              .toString()
-              .replace(/{youtube}/, youtube)
-              .replace(/{title}/, title ? title : author)
-              .replace(/{author}/, title ? author : "")
-              .replace(/{body}/, body);
-          })
-          }</div>`.replace(/\n\s+/g, "\n");
-        }
-    });
-    nav += "</ul>\n</li>";
-    try{
-    fs.mkdirSync("./tmp");
-    } catch(o_O){
-      console.log(o_O);
-    }
-    fs.writeFileSync("./tmp/navigation.html", nav);
-    fs.writeFileSync("./tmp/index.md", res);
-});
-
-gulp.task("chuiten", function() {
-  const template = fs.readFileSync("src/html/chuiten.html")
-    .toString()
-    .split("${contents}");
-
-  return gulp.src("./注意点/*.md")
-    .pipe($.markdown())
-    .pipe($.rename({extname: ""}))
-    .pipe($.wrapper({
-      header: template[0],
-      footer: template[1]}))
-    .pipe($.concat("chuiten.txt"))
-    .pipe(gulp.dest("./tmp"));
-});
 
 gulp.task("images", () =>
   gulp.src("res/img/**/*")
