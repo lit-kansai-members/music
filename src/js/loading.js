8a0dff33dddacda59e51672cd9dc196475fa0468
Yugo Yagita <2000ygkn0713@gmail.com>
2017-03-02 22:19:39 +0900

    Fix loading

--- src/js/loading.js
+++ src/js/loading.js
@@ -1,21 +1,21 @@
 $("#mark")[0].style.transform = `translateX(${
-    $("#loading svg")[0].getBoundingClientRect.left - $("#mark")[0].getBoundingClientRect().left
+    $("#loading svg")[0].getBoundingClientRect().left - $("#mark")[0].getBoundingClientRect().left
   }px)`;
 
 
 const paths = Array.from(document.querySelectorAll("path.first, path.second"));
 
 Promise.resolve()
-.then(new Promise(res => document.addEventListener("load", e => res())))
-.then(new Promise(res => 
+.then(() => new Promise(res => window.addEventListener("load", e => res())))
+.then(() => new Promise(res => 
   paths[0].addEventListener("animationiteration", e =>{
-    if(!(e.elapsedTime % 4)) res()
+    if((e.elapsedTime % 4) < 2) res()
 })))
-.then(new Promise(res =>{
+.then(() => new Promise(res =>{
   paths.forEach(el => el.style.animationPlayState="paused")
   const loading = document.getElementById("loading");
   loading.style.opacity = "0";
-  loading.addEventListener("transitionend", e => res());
+  loading.addEventListener("transitionend", () => res());
 }))
-.then(document.body.classList.add("loaded"))
+.then(() => document.body.classList.add("loaded"));
 
