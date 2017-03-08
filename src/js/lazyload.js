f8a7c3a110b5964b9145cfc93101915e949afbc5
Yugo Yagita <2000ygkn0713@gmail.com>
2017-03-08 18:54:46 +0900

    Update lazyload.js

--- src/js/lazyload.js
+++ src/js/lazyload.js
@@ -1,7 +1,7 @@
 $(".play").forEach(el =>
   el.addEventListener("click", () => {
     const iframe = document.createElement("iframe");
-    iframe.src="https://www.youtube.com/embed/${this.dataset.youtubeId}?autoplay=1";
+    iframe.src=`https://www.youtube.com/embed/${this.dataset.youtubeId}?autoplay=1`;
     iframe.frameborder="0";
     iframe.allowfullscreen = true;
     el.parentElement.appendChild(iframe);
