ca8d3d4d4558e9c8e94b17d6cfe86ad9f84b1695
Yugo Yagita <2000ygkn0713@gmail.com>
2017-05-09 22:12:41 +0900

    Lighter in requestAnimationFrame function

--- src/js/parallax.js
+++ src/js/parallax.js
@@ -1,23 +1,44 @@
+const getDocumentOffset = e => e.getBoundingClientRect().top + pageYOffset;
+const viewportHeight = window.innerHeight;
+
 const $backgrounds = Array
   .from(document.querySelectorAll(".parallax"))
-  .map(container => ({
-    container,
-    background: Array.from(container.childNodes)
-      .find(e => e.classList && e.classList.contains("background"))})
-   );
+  .map(container => {
+    const background = Array.from(container.children)
+      .find(e => e.classList && e.classList.contains("background"));
+    return ({
+      container,
+      background,
+      top: getDocumentOffset(container),
+      containerHeight: container.offsetHeight,
+      backgroundHeight: background.offsetHeight
+    })
+  });
+
+window.addEventListener("resize", e =>{
+  $backgrounds = $backgrounds.map(value => {
+    value.top = getDocumentOffset(value.container);
+    value.containerHeight = value.container.offsetHeight;
+    value.backgroundHeight = value.background.offsetHeight;
+    return value;
+  })
+  viewportHeight = window.innerHeight;
+})
  
 const anim = () =>{
-  requestAnimationFrame(anim);
-  const viewportHeight = window.innerHeight;
+  const scrollY = pageYOffset;
   let i = 0;
   while( i < $backgrounds.length){
-    const {background, container} = $backgrounds[i];
-    const scroll = container.getBoundingClientRect().top;
-    background.style.transform = "translateY(" + 
-      -(scroll + (viewportHeight - scroll) / (viewportHeight + container.offsetHeight) * (background.offsetHeight - viewportHeight)) +
-    "px)";
+    const {background, top, containerHeight, backgroundHeight} = $backgrounds[i];
+    const viewportTop = top - pageYOffset;
+    if(!(viewportTop < 0 && viewportTop > viewportHeight)) {
+      background.style.transform = "translateY(" + 
+        -(viewportTop + (viewportHeight - viewportTop) / (viewportHeight + containerHeight) * (backgroundHeight - viewportHeight)) +
+      "px)";
+    }
     i = (i+1) | 0;
   }
+  requestAnimationFrame(anim);
 }
 
 anim();
