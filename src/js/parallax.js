edff5f7fef5a5306f310daa99f1d910d9d23f459
Yugo Yagita <2000ygkn0713@gmail.com>
2017-07-23 14:09:00 +0000

    Improve parallax animation

--- src/js/parallax.js
+++ src/js/parallax.js
@@ -1,7 +1,6 @@
 const getDocumentOffset = e => e.getBoundingClientRect().top + pageYOffset;
-const viewportHeight = window.innerHeight;
+let viewportHeight = window.innerHeight;
 
-let scrollY;
 let $backgrounds = Array
   .from(document.querySelectorAll(".parallax"))
   .map(container => {
@@ -9,38 +8,35 @@ let $backgrounds = Array
       .find(e => e.classList && e.classList.contains("background"));
     return ({
       container,
-      background,
-      top: getDocumentOffset(container),
-      containerHeight: container.offsetHeight,
-      backgroundHeight: background.offsetHeight
+      background
     })
   });
 
-window.addEventListener("resize", e =>{
-  $backgrounds = $backgrounds.map(value => {
-    value.top = getDocumentOffset(value.container);
-    value.containerHeight = value.container.offsetHeight;
-    value.backgroundHeight = value.background.offsetHeight;
-    return value;
-  })
-  viewportHeight = window.innerHeight;
-})
-
-window.addEventListener("scroll", e => scrollY = pageYOffset);
- 
-const anim = () =>{
-  let i = 0;
-  while( i < $backgrounds.length){
-    const {background, top, containerHeight, backgroundHeight} = $backgrounds[i];
-    const viewportTop = top - scrollY;
-    if(!(viewportTop < 0 && viewportTop > viewportHeight)) {
-      background.style.transform = "translateY(" + 
-        -(viewportTop + (viewportHeight - viewportTop) / (viewportHeight + containerHeight) * (backgroundHeight - viewportHeight)) +
-      "px)";
+const update = e =>
+  requestAnimationFrame(()=> {
+    if(e.type !== 'scroll') {
+      $backgrounds = $backgrounds.map(value => {
+        value.top = getDocumentOffset(value.container);
+        value.containerHeight = value.container.offsetHeight;
+        value.backgroundHeight = value.background.offsetHeight;
+        return value;
+      })
+      viewportHeight = window.innerHeight;
     }
-    i = (i+1) | 0;
-  }
-  requestAnimationFrame(anim);
-}
+    let i = 0;
+    while( i < $backgrounds.length){
+      const {background, top, containerHeight, backgroundHeight} = $backgrounds[i];
+      const viewportTop = top - window.pageYOffset;
+      if(!(viewportTop < 0 && viewportTop > viewportHeight)) {
+        background.style.transform = "translateY(" + 
+          -(viewportTop + (viewportHeight - viewportTop) / (viewportHeight + containerHeight) * (backgroundHeight - viewportHeight)) +
+        "px)";
+      }
+      i = (i+1) | 0;
+    }
+  });
 
-anim();
+addEventListener('scroll', update);
+addEventListener('resize', update);
+addEventListener('orientationchange', update);
+addEventListener('load', update);
