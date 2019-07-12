43e67a7e4c123630d8d274f981b357bb04e905dd
Yugo Yagita <2000ygkn0713@gmail.com>
2019-07-12 16:24:22 +0900

    add pug config

---
+++ src/pug/pug.config.js
@@ -0,0 +1,19 @@
+const marked = require("marked");
+const YAML = require("js-yaml");
+const fs = require("fs");
+
+module.exports = {
+  locals: {
+    data: {
+      marked,
+      years: YAML.safeLoad(fs.readFileSync("./index.yml")),
+      chuitens: fs
+        .readdirSync("./注意点")
+        .filter(p => /.+\.md$/.test(p))
+        .map(p => ({
+          content: fs.readFileSync("./注意点/" + p).toString(),
+          name: p.slice(0, -3)
+        }))
+    }
+  }
+};
